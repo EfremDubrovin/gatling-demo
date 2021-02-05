333d65a841391fbedb8635b48fb9b5329623e104
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2021-02-05 13:56:06 +0200

    add code reuse with methods

---
+++ src/test/scala/gatling/demo/CodeReuseScenario.scala
@@ -0,0 +1,36 @@
+package gatling.demo
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+/**
+ * Created by Efrem Dubrovin - Delta Source Bulgaria on 05/02/2021.
+ */
+class CodeReuseScenario extends Simulation {
+
+	def getAllVideoGames() = {
+		exec(http("Get all video games")
+			.get("videogames")
+			.check(status.is(200)))
+	}
+
+	def getASpecificVideoGame() = {
+		exec(http("Get specific video game")
+			.get("videogames/1")
+			.check(status.in(200, 210)))
+	}
+
+	val scn = scenario("Video game DB")
+		.exec(getAllVideoGames())
+		.pause(5)
+		.exec(getASpecificVideoGame())
+		.pause(5)
+
+	setUp(scn.inject(atOnceUsers(1))
+		.protocols(http
+			.baseUrl("http://localhost:8080/app/")
+			.acceptHeader("application/json")))
+
+}
