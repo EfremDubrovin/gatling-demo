e60ed8b12a35a605e25049f853fb5ef1f1cd1657
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2021-02-05 13:02:24 +0200

    add jsonPath check for response data

---
+++ src/test/scala/GatlingRunner.scala
@@ -0,0 +1,20 @@
+import gatling.demo.{CheckJsonPathScenario, DemoScenario}
+import io.gatling.app.Gatling
+import io.gatling.core.config.GatlingPropertiesBuilder
+
+/**
+ * Main runner class - runs a specific scenario class
+ *
+ * Created by Efrem Dubrovin - Delta Source Bulgaria on 05/02/2021.
+ */
+object GatlingRunner {
+
+	def main(args: Array[String]): Unit = {
+		val simClass = classOf[CheckJsonPathScenario].getName
+		val props = new GatlingPropertiesBuilder
+		props.simulationClass(simClass)
+
+		Gatling.fromMap(props.build)
+	}
+
+}
--- src/test/scala/gatling/demo/CheckJsonPathScenario.scala
+++ src/test/scala/gatling/demo/CheckJsonPathScenario.scala
@@ -8,25 +8,14 @@ import scala.concurrent.duration.DurationInt
 /**
  * Created by Efrem Dubrovin - Delta Source Bulgaria on 05/02/2021.
  */
-class DemoScenario extends Simulation {
+class CheckJsonPathScenario extends Simulation {
 
 	val scn = scenario("Video game DB")
-		.exec(http("Get all video games")
-			.get("videogames")
-			.check(status.is(200)))
-		.pause(5)
 
 		.exec(http("Get specific video game")
 			.get("videogames/1")
-			.check(status.in(200, 210))
+			.check(jsonPath("$.name").is("Resident Evil 4"))
 		)
-		.pause(1, 5)
-
-		.exec(http("Get all video games")
-			.get("videogames")
-			.check(status.not(404), status.not(500))
-		)
-		.pause(3000.milliseconds)
 
 	setUp(scn.inject(atOnceUsers(1))
 		.protocols(http
